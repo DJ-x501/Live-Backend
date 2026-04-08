dc9268aa8950cc936a74d95e15cb2f268e47bc49
djx501 <bhargavdhananjaix501@gmail.com>
2026-04-08 23:15:29 +0530

    render configs

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require("./src/app");
 const {connectDB}  = require("./src/config/database")
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 6800;
 
-app.listen(port, async() => {
+app.listen(port, "0.0.0.0", async() => {
     await connectDB();
     console.log(`Server running on port ${port}`);
 });
